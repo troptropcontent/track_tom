245cbb3870843cba9c26236fdc4213307694847a
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-27 11:12:36 +0000

    create AuthentifiedRoute to make some routres privates

---
+++ packages/vite-project/src/routes/AuthentifiedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../components/auth/useAuth";
+import { ReactNode } from "react";
+
+const AuthentifiedRoute = ({children}: {children: ReactNode}) => {
+  const { token } = useAuth();
+
+  if (token === null) {
+    return <Navigate to={`/auth/login?redirectTo=${window.location.pathname}`} replace />
+  }
+    
+  return <>{children}</>
+};
+
+export default AuthentifiedRoute;
+
