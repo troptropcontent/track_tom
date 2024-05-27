33ad7b957109df648275ac76a32395f682b9549a
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-27 11:08:44 +0000

    allow requests to be done by the front end

--- packages/nestjs/src/main.ts
+++ packages/nestjs/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://127.0.0.1:3001',
+  });
   await app.listen(3000);
 }
 bootstrap();
