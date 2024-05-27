7b7bfea3efa26ff4d7081f8654c46713ad0baa5c
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-27 11:13:33 +0000

    create a router

---
+++ packages/vite-project/src/routes/Router.tsx
@@ -0,0 +1,25 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import AuthentifiedRoute from "./AuthentifiedRoute";
+import { Login } from "./auth/login";
+import { Register } from "./auth/register";
+import { Profile } from "./auth/profile/Index";
+import { Root } from "./root";
+
+const Router = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route path="/auth">
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<AuthentifiedRoute><Profile /></AuthentifiedRoute>} />
+        </Route>
+        <Route path="/" element={<AuthentifiedRoute><Root /></AuthentifiedRoute>} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
+
