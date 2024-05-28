7bcf831b3a80a2eb493b8f76ee6a90a4164be236
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-28 07:31:13 +0000

    load token in axios when present

--- packages/vite-project/src/components/auth/useAuth.tsx
+++ packages/vite-project/src/components/auth/useAuth.tsx
@@ -1,18 +1,23 @@
 import { useContext } from "react";
 import { AuthenticationContext } from "./provider";
+import axios from "axios";
 
 const useAuth = () => {
-  const {token, setToken} = useContext(AuthenticationContext);
+  const { token, setToken } = useContext(AuthenticationContext);
+  if (typeof token == "string") {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } 
   return {
     token,
     login: (token: string) => {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setToken(token);
     },
     logout: () => {
+      delete axios.defaults.headers.common["Authorization"];
       setToken(null);
     },
   };
 };
 
 export { useAuth };
-
