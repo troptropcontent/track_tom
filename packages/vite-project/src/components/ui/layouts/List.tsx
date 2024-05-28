f60cc7e27957483b5c6e32c57489697a1b0a846e
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-28 17:54:53 +0000

    create the List component

--- packages/vite-project/src/components/ui/layouts/List.tsx
+++ packages/vite-project/src/components/ui/layouts/List.tsx
@@ -1,29 +1,69 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 import { Link } from "../navigation/link";
 import { Box } from "./box";
 import { Heading } from "../typography/heading";
 import { Classes } from "../constants";
 type ListProps = {
-  children: ReactNode, 
-  gap?: keyof typeof Classes.gap
+  children: ReactNode;
+  gap?: keyof typeof Classes.gap;
   emptyState: {
-    text: string
-    action: Parameters<typeof Link>[0]
-  }
-} 
+    text: string;
+    action: Parameters<typeof Link>[0];
+  };
+};
 
-const EmptyState = ({text, action}: ListProps["emptyState"]) => {
-  return <Box flex flexDirection="col" justify="start" items="center" padding="lg">
-    <Heading size="lg">{text}</Heading>
-    <Link {...action} />
-  </Box>
-}
+type EmptyStateProps = {
+  text: string;
+  action: Parameters<typeof Link>[0];
+};
+const EmptyState = ({ text, action }: EmptyStateProps) => {
+  return (
+    <Box
+      flex
+      flexDirection="col"
+      justify="start"
+      items="center"
+      padding="lg"
+      className={"hidden only:flex"}
+    >
+      <Heading size="lg">{text}</Heading>
+      <Link {...action} />
+    </Box>
+  );
+};
 
-const List = ({children, gap = "md", emptyState}: ListProps) => {
-  return <Box flex flexDirection="col" gap={gap} items="stretch" justify="start">
-    <EmptyState {...emptyState} />
-    {children}
-  </Box>
-}
+type ContainerProps = {
+  children: ReactNode;
+  gap?: keyof typeof Classes.gap;
+};
+const Container = ({ children, gap = "md" }: ContainerProps) => {
+  return (
+    <Box
+      as="ul"
+      flex
+      flexDirection="col"
+      gap={gap}
+      items="stretch"
+      justify="start"
+    >
+      {children}
+    </Box>
+  );
+};
 
-export {List}
+type ItemProps = Parameters<typeof Box>[0];
+const Item = ({ children, ...props }: ItemProps) => {
+  return (
+    <Box {...props} as="li">
+      {children}
+    </Box>
+  );
+};
+
+const List = {
+  Container,
+  Item,
+  EmptyState,
+};
+
+export { List };
