b7da5a93fc417e3dcd6dc926b0a8e1c15f9e9753
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-28 18:00:43 +0000

    update the root

--- packages/vite-project/src/routes/root.tsx
+++ packages/vite-project/src/routes/root.tsx
@@ -1,35 +1,40 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { Api } from "../api";
-import { useAuth } from "../components/auth/useAuth";
 import { List } from "../components/ui/layouts/List";
-import LoadingWrapper from "../components/ui/layouts/LoadingWrapper";
+import {LoadingWrapper} from "../components/ui/layouts/LoadingWrapper";
 import { Box } from "../components/ui/layouts/box";
-import { Link } from "../components/ui/navigation/link";
 import { Heading } from "../components/ui/typography/heading";
 
 const Root = () => {
-  const { logout } = useAuth();
-  const { data: projects = [], isLoading } = Api.projects.useProjectsQuery()
-  
+  const { data: projects = [], isLoading } = Api.projects.useProjectsQuery();
+  const navigate = useNavigate();
+
   return (
-    <Box flex flexDirection="col" items="stretch" justify="between" padding="sm">
-      <Box flex flexDirection="row" items="end" justify="between" padding="sm">
-        <Heading>Home</Heading>
-        <Link onClick={logout}>Logout</Link>
-      </Box>
+    <Box
+      flex
+      flexDirection="col"
+      items="stretch"
+      justify="between"
+      padding="sm"
+    >
       <LoadingWrapper isLoading={isLoading}>
-        <List emptyState={{
-          text: "No projects",
-          action: {
-            text: "Create project",
-            onClick: () => {}
-          }
-        }}>
+        <List.Container>
+          <List.EmptyState
+            text="No projects"
+            action={{
+              text: "Create project",
+              onClick: () => {
+                navigate("/projects/new");
+              },
+            }}
+          />
           {projects.map((project) => (
-            <Box key={project.id}>
+            <List.Item key={project.id}>
               <Heading>{project.name}</Heading>
-            </Box>
+            </List.Item>
           ))}
-        </List>
+        </List.Container>
       </LoadingWrapper>
     </Box>
   );
