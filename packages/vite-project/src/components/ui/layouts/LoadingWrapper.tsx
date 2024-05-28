41e865b03cb64c5de7fb3b596184e413a36ab877
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-28 17:56:29 +0000

    formater

--- packages/vite-project/src/components/ui/layouts/LoadingWrapper.tsx
+++ packages/vite-project/src/components/ui/layouts/LoadingWrapper.tsx
@@ -1,11 +1,20 @@
 import { Box } from "./box";
 
-const LoadingWrapper = ({ children, isLoading }: { children: React.ReactNode, isLoading: boolean }) => {
+const LoadingWrapper = ({
+  children,
+  isLoading,
+}: {
+  children: React.ReactNode;
+  isLoading: boolean;
+}) => {
   if (isLoading) {
-    return <Box flex flexDirection="row" items="center" justify="center">Loading...</Box>;
+    return (
+      <Box flex flexDirection="row" items="center" justify="center">
+        Loading...
+      </Box>
+    );
   }
   return <>{children}</>;
 };
 
 export default LoadingWrapper;
-
