e0ce1826d85afdfbc519cfcb7786fe39657ca539
Tom Ecrepont <tomecrepont@gmail.com>
2024-05-30 09:16:09 +0000

    update the "projects query data" right after the mutation

--- packages/vite-project/src/api/projects/create.tsx
+++ packages/vite-project/src/api/projects/create.tsx
@@ -1,14 +1,23 @@
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { BASE_URL } from "../constants";
 import axios from "axios";
 import { BaseProjectProperties, Project } from "./types";
 
 const createProject = (data: BaseProjectProperties) =>
   axios
-    .post<Project[]>(`${BASE_URL}/projects`, data)
+    .post<Project>(`${BASE_URL}/projects`, data)
     .then((response) => response.data);
 
-const useCreateProjectMutation = () =>
-  useMutation({ mutationFn: createProject });
+const useCreateProjectMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: createProject,
+    onSuccess: (newProject: Project) => {
+      queryClient.setQueryData("projects", (oldData: Project[] = []) => [...oldData, newProject]);
+      queryClient.invalidateQueries("projects");
+    },
+  });
+};
+  
 
 export { useCreateProjectMutation };
